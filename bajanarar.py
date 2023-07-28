a6811e825ebd49254a9f8404c6123b3073efc4eb
Saaten <136030812+Saaten@users.noreply.github.com>
2023-07-28 16:34:57 +0400

    Add files via upload

---
+++ bajanarar.py
@@ -0,0 +1,19 @@
+﻿'''ամենամեծ ընդհանուր բաժանարարը'''
+
+num1=int(input('Enter the first number'))
+num2=int(input('Enter the second number'))
+if num1<0 or num2<0:
+    print("Error")
+elif num1==0:
+    print('The greatest common divisor is ', num2)
+elif num2==0:
+    print('The greatest common divisor is ', num1)
+else:
+    k=1
+    for i in range(num1,num2):
+        if num1%i==0 and num2%i==0:
+            k=i
+    print("The greatest common divisor is ", k)
+
+
+
